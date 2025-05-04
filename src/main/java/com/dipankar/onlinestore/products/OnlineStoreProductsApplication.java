ec350d4c45ba998619e1ff5f4e029315e8906ea7
Dipankar Datta <dattapro5@gmail.com>
2025-05-04 23:46:21 +0530

    Initial commit

---
+++ src/main/java/com/dipankar/onlinestore/products/OnlineStoreProductsApplication.java
@@ -0,0 +1,13 @@
+package com.dipankar.onlinestore.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dipankar/onlinestore/products/OnlineStoreProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipankar.onlinestore.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
